fcaf14a3207bcc2b22040cc3f0aa27b5f3c94a58
Abmoviehouse <130202418+Abmoviehouse@users.noreply.github.com>
2023-08-25 18:10:25 +0545

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,9 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, IS_SHORTLINK, LOG_CHANNEL, TUTORIAL, GRP_LNK, CHNL_LNK, CUSTOM_FILE_CAPTION, SECOND_SHORTLINK_URL, SECOND_SHORTLINK_API
+from info import AUTH_CHANNEL, DIRECT_GEN, DIRECT_GEN_DB, DIRECT_GEN_URL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM, SHORTLINK_URL, SHORTLINK_API, IS_SHORTLINK, LOG_CHANNEL, TUTORIAL, GRP_LNK, CHNL_LNK, CUSTOM_FILE_CAPTION, SECOND_SHORTLINK_URL, SECOND_SHORTLINK_API
 from imdb import Cinemagoer 
 import asyncio
+from urllib.parse import quote_plus
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from pyrogram import enums
@@ -734,7 +735,66 @@ async def check_verification(bot, userid):
             return True
     else:
         return False
-    
+
+#Direct Link Generator
+async def direct_gen_handler(m: Message):
+    if not DIRECT_GEN:
+        return None, None
+    try:
+        log_msg = await m.copy(chat_id=DIRECT_GEN_DB)
+        stream_link, download_link = await gen_link(log_msg)
+        if stream_link and download_link:
+            if not m.reply_markup:
+                markup = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton("🎥 Stream/Fast Download ⚡", url=stream_link)
+                    ]
+                ]
+            )
+            else:
+                markup = m.reply_markup.inline_keyboard.copy()
+                markup.insert(
+                    0,
+                    [
+                        InlineKeyboardButton("🎥 Stream/Fast Download ⚡", url=stream_link)
+                    ]
+                )
+                markup = InlineKeyboardMarkup(markup)
+            return markup
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
+        await direct_gen_handler(m)
+
+# Direct Link Generator
+async def gen_link(log_msg):
+    page_link = f"{DIRECT_GEN_URL}watch/{get_hash(log_msg)}{log_msg.id}"
+    stream_link = f"{DIRECT_GEN_URL}{log_msg.id}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+    return page_link, stream_link
+
+def get_media_from_message(message: "Message"):
+    media_types = (
+        "audio",
+        "document",
+        "photo",
+        "sticker",
+        "animation",
+        "video",
+        "voice",
+        "video_note",
+    )
+    for attr in media_types:
+        if media := getattr(message, attr, None):
+            return media
+
+def get_name(media_msg: Message) -> str:
+    media = get_media_from_message(media_msg)
+    return getattr(media, "file_name", "None")
+
+
+def get_hash(media_msg: Message) -> str:
+    media = get_media_from_message(media_msg)
+    return getattr(media, "file_unique_id", "")[:6]
     
 async def send_all(bot, userid, files, ident, chat_id, user_name, query):
     settings = await get_settings(chat_id)
